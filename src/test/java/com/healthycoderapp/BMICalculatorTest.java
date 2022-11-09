4b60abd7f40ad41054ec89bf148f15c90d13676a
mjunior6 <mauri.junior@capgemini.com>
2022-11-09 11:00:24 -0300

    5. First unit test

---
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,14 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @Test
+    void test(){
+       assertTrue(BMICalculator.isDietRecommended(89.0, 1.72));
+    }
+
+}
