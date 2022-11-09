3c3a3a24e7c4ba42e126eac2e02848c1db6e2979
mjunior6 <mauri.junior@capgemini.com>
2022-11-09 11:36:14 -0300

    11. Test array equality

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -71,4 +71,17 @@ class BMICalculatorTest {
         assertNull(coderWorstBMI);
     }
 
+    @Test
+    void should_ReturnCorrectBMIScoreArray_When_CoderListNotEmpty(){
+        // given
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80, 60.0));
+        coders.add(new Coder(1.82, 98.0));
+        coders.add(new Coder(1.82,64.7));
+        double[] expected = {18.52, 29.59, 19.53};
+        // when
+        double[] bmiScores = BMICalculator.getBMIScores(coders);
+        // then
+        assertArrayEquals(expected, bmiScores);
+    }
 }
