e77569e5bb88d78c8147050982d5b66b8a403ac6
mjunior6 <mauri.junior@capgemini.com>
2022-11-09 11:25:08 -0300

    9. Test with multiple assertions

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -3,6 +3,9 @@ package com.healthycoderapp;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
@@ -22,15 +25,17 @@ class BMICalculatorTest {
     @Test
     // always test the opposite result of logic
     void shouldReturnFalseWhenDietNotRecommended() {
+        // given
         double weight = 50.0;
         double height = 1.92;
+        // when
         boolean recommended = BMICalculator.isDietRecommended(weight, height);
+        // then
         assertFalse(recommended);
     }
 
     @Test
-        // always test the opposite result of logic
-    void shouldThrowAritmeticExceptionWhenHeightZero() {
+    void shouldThrowArithmeticExceptionWhenHeightZero() {
         // given
         double weight = 50.0;
         double height = 0.0;
@@ -39,4 +44,21 @@ class BMICalculatorTest {
         // then
         assertThrows(ArithmeticException.class, executable);
     }
+
+    @Test
+    void shouldReturnCoderWithWorstBMIWhenCoderListNotEmpty(){
+        // given
+        List<Coder> coders = new ArrayList<>();
+        coders.add(new Coder(1.80, 60.0));
+        coders.add(new Coder(1.82, 98.0));
+        coders.add(new Coder(1.82,64.7));
+        // when
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+        // then
+        assertAll(
+            () -> assertEquals(1.82, coderWorstBMI.getHeight()),
+            () -> assertEquals(98.0, coderWorstBMI.getWeight())
+        );
+    }
+
 }
